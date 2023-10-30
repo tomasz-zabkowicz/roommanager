0fd4a95e229183aa9dc0c151adf773b6fb735231
Tom Zabkowicz <tomasz.zabkowicz@itcrowd.pl>
2023-10-30 15:56:23 +0100

    Initial project structure

---
+++ src/main/java/pl/beusable/roommanager/RoomManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.beusable.roommanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomManagerApplication.class, args);
+	}
+
+}
